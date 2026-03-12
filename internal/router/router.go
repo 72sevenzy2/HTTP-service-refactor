10d30b9f26b1e2dbb0e48a53e27a8b8125edfc34
72 <72sevenzy2@gmail.com>
2026-03-12 13:08:27 +0500

    working on implementing a router to handle routes, downloaded necessary dependencies (chi router)

---
+++ internal/router/router.go
@@ -0,0 +1,9 @@
+package router
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/72sevenzy2/golang-API/internal/handler"
+	"github.com/72sevenzy2/golang-API/internal/service"
+)
