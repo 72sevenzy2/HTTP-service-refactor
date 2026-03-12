3f2de3d9e49c2b4f02b47267d74b8994d2e58d3b
72 <72sevenzy2@gmail.com>
2026-03-12 13:17:59 +0500

    finished router

--- internal/router/router.go
+++ internal/router/router.go
@@ -6,4 +6,17 @@ import (
 
 	"github.com/72sevenzy2/golang-API/internal/handler"
 	"github.com/72sevenzy2/golang-API/internal/service"
+	"github.com/72sevenzy2/golang-API/internal/health"
 )
+
+func NewRouter(service service.Greeter) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/greet", handler.GreetHandler(service))
+	r.Get("/health", health.HealthHandler())
+
+	return r
+}
