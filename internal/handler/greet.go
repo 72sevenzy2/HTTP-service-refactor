c1576a3f96f4469b374e78fcc8cabdc1af10b62a
72 <72sevenzy2@gmail.com>
2026-03-16 17:24:04 +0500

    updated json folder name + import from greet handler

--- internal/handler/greet.go
+++ internal/handler/greet.go
@@ -3,24 +3,27 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
 	"github.com/72sevenzy2/golang-API/internal/service"
+	"github.com/72sevenzy2/golang-API/internal/response"
 )
+
+type GreetResponse struct {
+	Message string `json:"name"`
+	Count int `json:"count"`
+}
+
+type GreetRequest struct {
+	Name string `json:"name"`
+}
 
 func GreetHandler(g service.Greeter) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// check if method is GET or not
-		if r.Method != http.MethodGet {
-			http.Error(w, "invalid method", http.StatusMethodNotAllowed)
-			return
-		}
-		name := r.URL.Query().Get("name")
-		resp, err := g.Greet(name)
+		var req GreetRequest
 
+		err := json.NewDecoder(r.Body).Decode(req)
 		if err != nil {
-			http.Error(w, "bad request", http.StatusBadRequest)
-			return
+			json.
 		}
-
-		fmt.Fprintln(w, resp)
 	}
 }
---
+++ internal/response/json.go
@@ -0,0 +1,21 @@
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func Json(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("content-type", "application/json")
+	w.WriteHeader(status)
+
+	json.NewEncoder(w).Encode(data)
+}
+
+func Error(w http.ResponseWriter, status int, message string) {
+	res := map[string]string{
+		"error": message,
+	}
+
+	Json(w, status, res)
+}
